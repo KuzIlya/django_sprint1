95899fe968274c7b15fe06e9e5e63c21b7f81462
Кузнецов Илья <KuznetsovIlyaDM@yandex.ru>
2023-09-16 00:35:23 +0300

    not checked version 1

---
+++ blogicum/blogicum/urls.py
@@ -0,0 +1,8 @@
+from django.urls import include, path
+
+urlpatterns = [
+    path('', include('blog.urls')),
+    path('posts/<int:id>/', include('blog.urls')),
+    path('category/<slug:category_slug>/', include('blog.urls')),
+    path('pages/', include('pages.urls')),
+]
